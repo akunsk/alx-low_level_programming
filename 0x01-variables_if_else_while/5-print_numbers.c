1b2eef4d96315ef77c69d0201cbf8715d560e980
akunsk <akunskingsley@gmail.com>
2022-06-03 10:43:58 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print integer from 0 to 10
+ *
+ * Description: return the required results
+ *
+ * Return: prints integer 0
+ *
+ */
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 10; var++)
+	{
+		printf("%d", var);
+	}
+	putchar('\n');
+	return (0);
+}	
