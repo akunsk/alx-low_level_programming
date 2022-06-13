dd53df15d6d1da7885c99957a98ba37b4fe820f5
akunsk <akunskingsley@gmail.com>
2022-06-13 01:56:17 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - prints every other character of a string, starting with the first.
+*
+* Return: 0.
+*/
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
