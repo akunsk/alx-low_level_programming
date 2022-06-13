d419bcba85cf609df9771e20d18e17be799c8b17
akunsk <akunskingsley@gmail.com>
2022-06-13 02:19:00 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,22 @@
-r
+#include<stdio.h>
+#include "main.h"
+/**
+ *print_array - prints elements of an array of integers.
+ *@a: value to be assessed.
+ *@n: value to be assessed.
+ *Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int p;
+
+	for (p = 0 ; p < n; p++)
+	{
+		printf("%d", a[p]);
+		if (p != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
