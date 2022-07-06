9320ecc88f34114ef98cd6770f16d682bfbe6d5f
akunsk <akunskingsley@gmail.com>
2022-07-06 15:26:27 -0700

    2-int_index.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
 * array_iterator - executes a function given as a parameter on each element
 * @array: array of integer
@@ -9,8 +10,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL || action == NULL)
-		return;
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array != NULL && size > 0 && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
